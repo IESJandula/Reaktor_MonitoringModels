02b47de5eea47f02f4e5508c19d3133b2336f7dc
Juan Sutil <jsutmes2712@g.educaand.es>
2024-02-11 13:17:18 +0100

    creacion entidades Action y Usb

---
+++ src/main/java/es/iesjandula/reaktor/models/Action.java
@@ -0,0 +1,29 @@
+package es.iesjandula.reaktor.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "action")
+public class Action
+{
+
+	@Id
+	@Column
+	private String name;
+	
+	@Column
+	private String command_Windows;
+	
+	@Column
+	private String command_Linux;
+	
+}
---
+++ src/main/java/es/iesjandula/reaktor/models/Usb.java
@@ -0,0 +1,33 @@
+package es.iesjandula.reaktor.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usb")
+public class Usb
+{
+
+	@Id
+	@Column
+	private Long id;
+	
+	@Column(nullable = false)
+	private Boolean active;
+	
+	@Column(nullable = false)
+	private String Type;
+	
+	@ManyToOne
+	Motherboard motherboard;
+	
+}
